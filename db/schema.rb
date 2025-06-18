295dfa3592b960c28df79c2334d58302e1802595
Marcelo Alexandre <marcelobalexandre@gmail.com>
2025-06-18 18:47:44 -0300

    Add database schema

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+ActiveRecord::Schema[8.0].define(version: 20250618203360) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "token", null: false
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  add_foreign_key "sessions", "users"
+end
