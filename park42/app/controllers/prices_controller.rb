2da2887e73d60674f426428ec049c8a9c4d2f3cd
Marcelo Alexandre <marcelobalexandre@gmail.com>
2025-06-19 14:39:05 -0300

    Document Authentication header for /price

---
+++ park42/app/controllers/prices_controller.rb
@@ -0,0 +1,15 @@
+class PricesController < ApplicationController
+  DAILY_FEE = 2_500
+
+  def create
+    params.require(%i[start_at end_at])
+    start_at = Time.zone.parse(params[:start_at])
+    end_at = Time.zone.parse(params[:end_at])
+
+    total_days = (end_at.to_date - start_at.to_date).to_i
+    total_days = 0 if total_days.negative?
+    total_price = total_days * DAILY_FEE
+
+    render json: { price: total_price, currency: "BRL" }
+  end
+end
--- park42/config/routes.rb
+++ park42/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
   resource :session, only: [ :create ]
+  resource :price, only: [ :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
