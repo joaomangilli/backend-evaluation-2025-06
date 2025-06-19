40bbf28b0e25770a6fc0cbf0a9f10a822f88d030
Marcelo Alexandre <marcelobalexandre@gmail.com>
2025-06-19 15:34:56 -0300

    Add Redis and Sidekiq for background jobs

--- park42/config/application.rb
+++ park42/config/application.rb
@@ -28,5 +28,8 @@ module Park42
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use Sidekiq backed by Redis for background jobs
+    config.active_job.queue_adapter = :sidekiq
   end
 end
